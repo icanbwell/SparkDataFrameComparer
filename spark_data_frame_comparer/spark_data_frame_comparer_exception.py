083c268bc664ef9bfb47f2feeadaee0b70da8f3e
imranq2 <imranq2@hotmail.com>
2020-12-08 12:27:58 -0800

    remove compare path if schema does not match

--- spark_data_frame_comparer/spark_data_frame_comparer_exception.py
+++ spark_data_frame_comparer/spark_data_frame_comparer_exception.py
@@ -1,10 +1,18 @@
 from pathlib import Path
 from typing import Optional, Union, Callable
+from enum import Enum
+
+
+class ExceptionType(Enum):
+    SchemaMismatch = 1
+    RowMismatch = 2
+    DataMismatch = 3
 
 
 class SparkDataFrameComparerException(Exception):
     def __init__(
         self,
+        exception_type: ExceptionType,
         result: str,
         expected: str,
         message: str,
@@ -15,6 +23,7 @@ class SparkDataFrameComparerException(Exception):
         func_path_modifier: Optional[Callable[[Union[Path, str]],
                                               Union[Path, str]]] = None
     ) -> None:
+        self.exception_type: ExceptionType = exception_type
         self.result: str = result
         self.expected: str = expected
         self.message: str = message
--- tests/simple_fail/test_simple_fail_with_func_path_modifier.py
+++ tests/simple_fail/test_simple_fail_with_func_path_modifier.py
@@ -4,10 +4,11 @@ from shutil import rmtree
 
 import pytest
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import lit
+from pyspark.sql.functions import lit, col
+from pyspark.sql.types import IntegerType
 
 from spark_data_frame_comparer.spark_data_frame_comparer import assert_compare_data_frames
-from spark_data_frame_comparer.spark_data_frame_comparer_exception import SparkDataFrameComparerException
+from spark_data_frame_comparer.spark_data_frame_comparer_exception import SparkDataFrameComparerException, ExceptionType
 from tests.conftest import clean_spark_session
 
 
@@ -25,8 +26,12 @@ def test_can_compare_simple_fail_with_func_path_modifier(
     mkdir(temp_folder)
 
     df1: DataFrame = spark_session.read.json(test_file_path)
+    df1 = df1.withColumn(
+        "simple_field",
+        col("simple_field").cast(IntegerType())
+    )
     df2: DataFrame = spark_session.read.json(test_file_path)
-    df2 = df2.withColumn("simple_field", lit("1"))
+    df2 = df2.withColumn("simple_field", lit(2).cast(IntegerType()))
 
     # Act
     result_path = temp_folder.joinpath("result.json")
@@ -45,11 +50,13 @@ def test_can_compare_simple_fail_with_func_path_modifier(
                 Path(str(x).replace(str(data_dir), "/foo/"))
             )
         except SparkDataFrameComparerException as e:
+            print(str(e))
             assert e.result_path == Path("/foo/temp/result.json")
             assert e.expected_path == Path("/foo/input/complex.json")
             assert e.compare_path == Path(
                 "/foo/temp/compare_complex.json.command"
             )
+            assert e.exception_type == ExceptionType.DataMismatch
             raise
 
     assert path.exists(data_dir.joinpath("temp/compare_complex.json.command"))
