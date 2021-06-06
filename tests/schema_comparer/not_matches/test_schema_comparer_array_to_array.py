5d63b50778134d87d3a38c98f32031317a685789
imranq2 <imranq2@hotmail.com>
2021-06-06 12:14:02 -0600

    improve test

--- tests/schema_comparer/not_matches/test_schema_comparer_array_to_array.py
+++ tests/schema_comparer/not_matches/test_schema_comparer_array_to_array.py
@@ -7,10 +7,14 @@ from pyspark.sql.types import (
     ArrayType,
 )
 
-from spark_data_frame_comparer.schema_comparer import SchemaComparer
+from spark_data_frame_comparer.schema_comparer import (
+    SchemaComparer,
+    SchemaComparerResult,
+    SchemaCompareErrorType,
+)
 
 
-def test_schema_comparer_array_to_array() -> None:
+def test_schema_comparer_array_to_array_not_match() -> None:
     print("")
     source_schema: StructType = StructType(
         [
@@ -24,8 +28,19 @@ def test_schema_comparer_array_to_array() -> None:
             StructField("age", StringType()),
         ]
     )
-    SchemaComparer.compare_schema(
-        parent_column_name=None,
+    result: SchemaComparerResult = SchemaComparer.compare_schema(
+        parent_column_name="identifier",
         source_schema=source_schema,
         desired_schema=desired_schema,
     )
+
+    for error in result.errors:
+        print(error)
+
+    assert len(result.errors) == 2, result.errors
+
+    assert result.errors[0].error_type == SchemaCompareErrorType.ERROR
+    assert result.errors[0].column == "identifier.name"
+
+    assert result.errors[1].error_type == SchemaCompareErrorType.INFO
+    assert result.errors[1].column == "identifier.age"
